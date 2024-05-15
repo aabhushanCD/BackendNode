057ff6e05607d3823cfe791ccba36557208c6614
aabhushanCD <dammardhakal426@gmail.com>
2024-05-15 20:39:21 +0545

    User completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandeler } from "../utils/asyncHandeler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-
+import { ApiResponse } from "../utils/ApiRespose.js";
 import { uploadOnCloudnary } from "../utils/cloudinary.js";
 const registerUser = asyncHandeler(async (req, res) => {
   //get user detail
@@ -52,6 +52,16 @@ const registerUser = asyncHandeler(async (req, res) => {
     email,
     coverImg: coverImg?.url || "",
   });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res.status(201).json(new ApiResponse(200,createdUser,"User registered Successfully"))
+
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,7 +51,8 @@ const userSchema = new Schema(
 );
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password =await bcrypt.hash(this.password, 10)
+  next()
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
